f8ca26def0d582036ae07c7ab3635e83f3337f14
Matte2322 <matthewchen1688@gmail.com>
2023-04-03 18:47:08 -0700

    C'MON WORK

--- translate.py
+++ translate.py
@@ -1,9 +1,10 @@
 import argostranslate.package
 import argostranslate.translate
 
-from_code = 'en'
-to_code = 'es'
+from_code = "en"
+to_code = "es"
 
+# Download and install Argos Translate package
 argostranslate.package.update_package_index()
 available_packages = argostranslate.package.get_available_packages()
 package_to_install = next(

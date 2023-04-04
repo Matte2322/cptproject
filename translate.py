f64df853ce394ab692106b63593049a3557dbaf5
Matte2322 <matthewchen1688@gmail.com>
2023-04-03 18:45:02 -0700

    spacing

--- translate.py
+++ translate.py
@@ -1,6 +1,9 @@
 import argostranslate.package
 import argostranslate.translate
 
+from_code = 'en'
+to_code = 'es'
+
 argostranslate.package.update_package_index()
 available_packages = argostranslate.package.get_available_packages()
 package_to_install = next(
